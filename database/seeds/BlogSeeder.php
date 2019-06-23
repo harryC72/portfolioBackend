7f58762df1026d66365289834ae55c317b5ef803
HarryC72 <haraldcarlsten@yahoo.se>
2019-06-23 21:06:25 +0200

    first commit portfolio backend

---
+++ database/seeds/BlogSeeder.php
@@ -0,0 +1,22 @@
+
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BlogSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        BlogPost::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        
+
+    }
+}
